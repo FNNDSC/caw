a1a75b68a2966fb996b4b3da9fa1af3c97b59d2a
Jennings Zhang <jenni_zh@protonmail.com>
2021-05-30 15:17:09 -0400

    Increase version 0.3.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(path.dirname(path.abspath(__file__)), 'README.md')) as f:
 # noinspection PyPackageRequirements
 setup(
     name='caw',
-    version='0.2.0-rc.1',
+    version='0.3.0',
     packages=['caw', 'chris'],
     url='https://github.com/FNNDSC/caw',
     license='MIT',
