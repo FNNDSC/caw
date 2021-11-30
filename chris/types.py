f04cb64e75ead10feeb599a3d9b6ffc78ee35aa1
Jennings Zhang <jenni_zh@protonmail.com>
2021-11-30 15:44:38 -0500

    Feeds

--- chris/types.py
+++ chris/types.py
@@ -1,6 +1,38 @@
-from typing import NewType
+from typing import NewType, Literal, Union
 
 CUBEToken = NewType('CUBEToken', str)
 CUBEAddress = NewType('CUBEAddress', str)
 CUBEUsername = NewType('CUBEUsername', str)
 CUBEPassword = NewType('CUBEPassword', str)
+
+CUBEUrl = NewType('CUBEUrl', str)
+
+PluginId = NewType('PluginId', int)
+PluginName = NewType('PluginName', str)
+PluginVersion = NewType('PluginVersion', str)
+PluginType = Literal['ds', 'fs', 'ts']
+
+SwiftPath = NewType('SwiftPath', str)
+
+ISOFormatDateString = NewType('ISOFormatDateString', str)
+
+# TODO: PluginInstanceStatus should be an enum
+PluginInstanceStatus = Literal[
+    'created', 'waiting', 'scheduled', 'started', 'registeringFiles',
+    'finishedSuccessfully', 'finishedWithError', 'cancelled'
+]
+
+CUBEErrorCode = NewType('CUBEErrorCode', str)
+
+ContainerImageTag = NewType('ContainerImageTag', str)
+
+FeedId = NewType('FeedId', int)
+PipingId = NewType('PipingId', int)
+PipelineId = NewType('PipelineId', int)
+
+FilesUrl = NewType('FilesUrl', CUBEUrl)
+
+ParameterType = Union[str, int, float, bool]
+ParameterTypeName = Literal['string', 'integer', 'float', 'boolean']
+PipelineParameterId = NewType('ParameterLocalId', int)
+PluginParameterId = NewType('ParameterGlobalId', int)
